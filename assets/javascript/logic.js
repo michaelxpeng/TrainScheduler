a5a45911154f240bc80d4e273e26d8ea07ea1a3a
mpeng <michaelxpeng@gmail.com>
2018-06-30 20:31:28 -0400

    basic files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAp5ZDEWmOVN1IRkCj5MQEulllj6YrGhFE",
+    authDomain: "trainscheduler-c96f1.firebaseapp.com",
+    databaseURL: "https://trainscheduler-c96f1.firebaseio.com",
+    projectId: "trainscheduler-c96f1",
+    storageBucket: "",
+    messagingSenderId: "279919886328"
+  };
+  firebase.initializeApp(config);
